da7af7e2ec0debb57a4e13011dadc25c57d340aa
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-10 08:42:19 +0530

    Consumer context

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import LanguageContext from "../context/LanguageContext";
 class Button extends Component {
-  static contextType = LanguageContext;
+  // static contextType = LanguageContext; method:1
   render() {
-    const text = this.context === "english" ? "Submit" : "Voorleggen";
-    return <button className="ui button primary">{text}</button>;
+    // const text = this.context === "english" ? "Submit" : "Voorleggen"; method:1
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {(value) => (value === "english" ? "Submit" : "Voorleggen")}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 export default Button;
