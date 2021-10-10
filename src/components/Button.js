47813197547ed0607f270e6b90c70e33255333a2
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-10 08:56:16 +0530

    implement used helper function for jsx

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,17 +3,20 @@ import LanguageContext from "../context/LanguageContext";
 import colorContext from "../context/colorContext";
 class Button extends Component {
   // static contextType = LanguageContext; method:1
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {(value) => (value === "english" ? "Submit" : "Voorleggen")}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
   render() {
     // const text = this.context === "english" ? "Submit" : "Voorleggen"; method:1
     return (
       <colorContext.Consumer>
-        {(color) => (
-          <button className={`ui button ${color}`}>
-            <LanguageContext.Consumer>
-              {(value) => (value === "english" ? "Submit" : "Voorleggen")}
-            </LanguageContext.Consumer>
-          </button>
-        )}
+        {(color) => this.renderButton(color)}
       </colorContext.Consumer>
     );
   }
